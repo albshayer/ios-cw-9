16b87ddcb07f9f7ca7ba4ae65ac8447cbbf05733
albshayer <bedourb02@gmail.com>
2020-10-10 23:30:24 +0300

    Done with cw p1

---
+++ ios-cw9-p1/ios-cw9-p1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-cw9-p1
+//
+//  Created by Bedour Albshayer on 10/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
