099e8d0c2462285b6fd37fd0ca595a5c4fcd6b1a
albshayer <bedourb02@gmail.com>
2020-10-10 20:23:02 +0300

    Done

---
+++ mycell.swift
@@ -0,0 +1,16 @@
+//
+//  mycell.swift
+//  iOSClaswork8
+//
+//  Created by Bedour Albshayer on 10/10/20.
+//  Copyright © 2020 Safeyah Coding. All rights reserved.
+//
+
+import UIKit
+
+class mycell: UITableViewCell {
+
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var cellLabel: UILabel!
+    
+}
